958e6517c7d460c26b2451c82ac7386fe7e6bddd
zyf584352700 <584352700@qq.com>
2016-04-21 22:29:18 +0800

    第一次提交

---
+++ Teacher.java
@@ -0,0 +1,44 @@
+package com.kj34.ch13;
+
+
+
+/**
+  学生类是用来创建老师对象的，
+  学生对象是用来存储该老师的信息的
+*/
+public class Teacher{
+	private long id;
+	private String name;
+	private int age;
+
+	public Teacher(){
+	
+	}
+	public Teacher(long id,String name,int age){
+		this.id = id;
+		this.name = name;
+		this.age = age;
+	}
+	public void setId(long id){
+		this.id = id;
+	}
+	public long getId(){
+		return this.id;
+	}
+	public void setName(String name){
+		this.name = name;
+	}
+	public String getName(){
+		return this.name;
+	}
+	public void setAge(int age){
+		this.age = age;
+	}
+	public int getAge(){
+		return this.age;
+	}
+
+	public String toString(){
+		return "Student[id:"+this.id+",name:"+this.name+",age:"+this.age+"]";
+	}
+}
